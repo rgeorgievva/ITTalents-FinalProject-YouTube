e79f9e588724974c69d93c058c02e73546b3acac
Radostina Georgieva <rhgeorgieva97@gmail.com>
2019-12-20 11:27:31 +0200

    E/R diagram fixed and DBManager added

---
+++ YouTube/src/main/java/Demo.java
@@ -0,0 +1,11 @@
+import model.db.DBManager;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ YouTube/src/main/java/model/video/Video.java
@@ -0,0 +1,89 @@
+package model.video;
+
+import java.time.LocalDateTime;
+
+public class Video {
+
+    private long id;
+    private String title;
+    private String description;
+    private String videoUrl;
+    private String thumbnailUrl;
+    private long duration;
+    private LocalDateTime dateUploaded;
+    private long ownerId;
+    private long categoryId;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public LocalDateTime getDateUploaded() {
+        return dateUploaded;
+    }
+
+    public void setDateUploaded(LocalDateTime dateUploaded) {
+        this.dateUploaded = dateUploaded;
+    }
+
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
